2cc5cfc92762ed89814f4eadbdd6ebf1fa5bbc1d
Jotta-TI <108303965+Jotta-TI@users.noreply.github.com>
2022-08-17 23:26:10 -0300

    Update Usuario.php

--- Usuario.php
+++ Usuario.php
@@ -6,14 +6,14 @@
     $nome              = (isset($_POST['nome']) )?$_POST["nome"]: null;
     $prontuario        = (isset($_POST['prontuario']) )?$_POST["prontuario"]: null;
     $corpo_academico   = (isset($_POST ['corpo_academico']) )?$_POST["corpo_academico"]: null;
-    echo (" Esse é o seu nome ".$nome);
+    
   
   require_once "/home/hostdeprojetos/public_html/testejotati/system/models/Usuarios.class.php";
-    echo ("1 Passei");
+    
     $usuario = new Usuarios($id_tipo_usuario, $nome, $prontuario, $senha, $corpo_academico);
-    echo ("1 Passei");
+    
     $usuario->insert();
  
-   echo ("2 Passei");
+   echo ("Inserção feita com sucesso");
  
 ?>
